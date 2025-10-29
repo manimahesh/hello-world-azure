b653c9292de2fe5518a81d8de8b219231a7fd324
Mahesh Mani <manimahesh1@gmail.com>
2025-10-29 14:31:08 -0700

    oidc fixes 3

--- create-oidc-trust.sh
+++ create-oidc-trust.sh
@@ -1,12 +1,12 @@
 # Define your GitHub details
-ORG_NAME="manimahesh"
-REPO_NAME="hello-world-azure"
-BRANCH_NAME="main" # Use 'pull_request' for pull request triggers
+#ORG_NAME="manimahesh"
+#REPO_NAME="hello-world-azure"
+#BRANCH_NAME="main" # Use 'pull_request' for pull request triggers
 APP_ID=5f55da87-40c6-4f6b-9fbb-f5b130a7d28a
 
 
 # Subject Identifier format: repo:<ORG>/<REPO>:ref:refs/heads/<BRANCH>
-SUBJECT="repo:$ORG_NAME/$REPO_NAME:ref:refs/heads/$BRANCH_NAME"
+# SUBJECT="repo:$ORG_NAME/$REPO_NAME:ref:refs/heads/$BRANCH_NAME"
 
 # Create the Federated Credential
 az ad app federated-credential create --id $APP_ID --parameters credentials.json
