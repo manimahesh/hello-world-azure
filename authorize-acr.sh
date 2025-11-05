2bde6b92f9279bf199797f40f58064fcfae50728
Mahesh Mani <manimahesh1@gmail.com>
2025-11-05 07:27:18 -0800

    refactor

--- authorize-acr.sh
+++
@@ -1,9 +0,0 @@
-ACR_NAME="mmaniacr"
-ACR_ID=$(az acr show --name $ACR_NAME --query id -o tsv)
-APP_ID=fcb7fdd1-8f17-44f2-a951-cc851c7cb96b
-
-# Role for CI/CD to build and push images
-az role assignment create \
-  --assignee $APP_ID \
-  --role "AcrPush" \
-  --scope $ACR_ID
--- create-oidc-trust.sh
+++
@@ -1,5 +0,0 @@
-# Define your GitHub details
-APP_ID=fcb7fdd1-8f17-44f2-a951-cc851c7cb96b
-
-# Create the Federated Credential
-az ad app federated-credential create --id $APP_ID --parameters credentials.json
